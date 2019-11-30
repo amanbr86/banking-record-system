b4a3a42ed5a6b7c585f701a64d5235b98bf95ede
amanbr86 <57818675+amanbr86@users.noreply.github.com>
2019-11-30 13:37:17 +0300

    Add files via upload

---
+++ banking record system(1).cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+class account_query
+{
+private:
+    char account_number[20];
+    char firstName[10];
+    char lastName[10];
+    float total_Balance;
+public:
+    void read_data();
+    void show_data();
+    void write_rec();
+    void read_rec();
+    void search_rec();
+    void edit_rec();
+    void delete_rec();
+};
+void account_query::read_data()
+{
+    cout<<"\nEnter Account Number: ";
+    cin>>account_number;
+    cout<<"Enter First Name: ";
+    cin>>firstName;
+    cout<<"Enter Last Name: ";
+    cin>>lastName;
+    cout<<"Enter Balance: ";
+    cin>>total_Balance;
+    cout<<endl;
+}
+void account_query::show_data()
+{
+    cout<<"Account Number: "<<account_number<<endl;
+    cout<<"First Name: "<<firstName<<endl;
+    cout<<"Last Name: "<<lastName<<endl;
+    cout<<"Current Balance: Rs.  "<<total_Balance<<endl;
+    cout<<"-------------------------------"<<endl;
+}
+void account_query::write_rec()
+{
+    ofstream outfile;
+    outfile.open("record.bank", ios::binary|ios::app);
+    read_data();
+    outfile.write(reinterpret_cast<char *>(this), sizeof(*this));
+    outfile.close();
+}
+void account_query::read_rec()
+{
+    ifstream infile;
+    infile.open("record.bank", ios::binary);
+    if(!infile)
+    {
+        cout<<"Error in Opening! File Not Found!!"<<endl;
+        return;
+    }
+    cout<<"\n****Data from file****"<<endl;
+    while(!infile.eof())
+    {
+        if(infile.read(reinterpret_cast<char*>(this), sizeof(*this))>0)
+        {
+            show_data();
+        }
+    }
+    infile.close();
+}
+int main()
+{
+    account_query A;
+    int choice;
+    cout<<"***Acount Information System***"<<endl;
+    while(true)
+    {
+        cout<<"Select one option below ";
+        cout<<"\n\t1-->Add record to file";
+        cout<<"\n\t2-->Show record from file";
+        cout<<"\n\t6-->Quit";
+        cout<<"\nEnter your choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            A.write_rec();
+            break;
+        case 2:
+            A.read_rec();
+            break;
+        case 6:
+            exit(0);
+            break;
+        default:
+            cout<<"\nEnter corret choice";
+            exit(0);
+        }
+    }
+    system("pause");
+    return 0;
+}
