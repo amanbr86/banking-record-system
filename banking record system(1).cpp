ebc59622593fa3a891131016838aa01673c922bb
unknown <bubusamson3@gmail.com>
2019-11-30 17:21:43 +0300

    bugs fixed

--- banking record system(1).cpp
+++ banking record system(1).cpp
@@ -102,6 +102,9 @@ int main()
         cout<<"\n\t1-->Add record to file";
         cout<<"\n\t2-->Show record from file";
 		cout << "\n\t3-->Search Record from file";
+		cout << "\n\t4-->Update Record";
+		cout << "\n\t5-->Delete Record";
+		cout << "\n\t6-->Quit";
         cout<<"\n\t6-->Quit";
         cout<<"\nEnter your choice: ";
         cin>>choice;
