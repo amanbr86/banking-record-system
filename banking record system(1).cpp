148f68887414281f6d2fbde87aa89bb82b521217
unknown <amanuelhagos930@gmail.com>
2019-11-30 16:09:40 +0300

    ssd

--- banking record system(1).cpp
+++ banking record system(1).cpp
@@ -92,31 +92,35 @@ void account_query::search_rec()
 	infile.read(reinterpret_cast<char*>(this), sizeof(*this));
 	show_data();
 }
-void account_query::edit_rec()
+void account_query::delete_rec()
 {
 	int n;
-	fstream iofile;
-	iofile.open("record.bank", ios::in | ios::binary);
-	if (!iofile)
+	ifstream infile;
+	infile.open("record.bank", ios::binary);
+	if (!infile)
 	{
 		cout << "\nError in opening! File Not Found!!" << endl;
 		return;
 	}
-	iofile.seekg(0, ios::end);
-	int count = iofile.tellg() / sizeof(*this);
+	infile.seekg(0, ios::end);
+	int count = infile.tellg() / sizeof(*this);
 	cout << "\n There are " << count << " record in the file";
-	cout << "\n Enter Record Number to edit: ";
+	cout << "\n Enter Record Number to Delete: ";
 	cin >> n;
-	iofile.seekg((n - 1)*sizeof(*this));
-	iofile.read(reinterpret_cast<char*>(this), sizeof(*this));
-	cout << "Record " << n << " has following data" << endl;
-	show_data();
-	iofile.close();
-	iofile.open("record.bank", ios::out | ios::in | ios::binary);
-	iofile.seekp((n - 1)*sizeof(*this));
-	cout << "\nEnter data to Modify " << endl;
-	read_data();
-	iofile.write(reinterpret_cast<char*>(this), sizeof(*this));
+	fstream tmpfile;
+	tmpfile.open("tmpfile.bank", ios::out | ios::binary);
+	infile.seekg(0);
+	for (int i = 0; i<count; i++)
+	{
+		infile.read(reinterpret_cast<char*>(this), sizeof(*this));
+		if (i == (n - 1))
+			continue;
+		tmpfile.write(reinterpret_cast<char*>(this), sizeof(*this));
+	}
+	infile.close();
+	tmpfile.close();
+	remove("record.bank");
+	rename("tmpfile.bank", "record.bank");
 }
 int main()
 {
